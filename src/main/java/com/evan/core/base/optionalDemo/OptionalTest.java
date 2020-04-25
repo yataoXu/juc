caddcfb4e6d3bebff5d74ff22de8e7919587f5c1
xuyatao@seassoon.com <Xu171206>
2020-04-25 13:44:01 +0800

    optional 学习

--- src/main/java/com/evan/core/base/optionalDemo/OptionalTest.java
+++ src/main/java/com/evan/core/base/optionalDemo/OptionalTest.java
@@ -1,11 +1,13 @@
 package com.evan.core.base.optionalDemo;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import org.junit.Test;
 
 import java.util.Optional;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class OptionalTest {
 
@@ -16,6 +18,13 @@ public class OptionalTest {
         System.out.println(empty.isPresent());
         assertFalse(empty.isPresent());
     }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenCreateOptionalOfempty_theNull() {
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty.get());
+    }
+
 
     //使用静态方法 of() 创建一个 Optional 对象。
     @Test
@@ -38,7 +47,7 @@ public class OptionalTest {
     public void givenNonNull_whenCreatesNullable_thenCorrect() {
         String name = "baeldung";
         Optional<String> opt = Optional.ofNullable(name);
-        System.out.println(opt.isPresent());
+        System.out.println(opt.isPresent());  // true
         assertTrue(opt.isPresent());
     }
 
@@ -47,8 +56,54 @@ public class OptionalTest {
     public void givenNull_whenCreatesNullable_thenCorrect() {
         String name = null;
         Optional<String> opt = Optional.ofNullable(name);
-//        System.out.println(opt.get());
-        System.out.println(opt.isPresent());
+        System.out.println(opt.isPresent()); // flase
         assertFalse(opt.isPresent());
     }
+
+
+    // 访问 Optional 对象的值
+
+    //从 Optional 实例中取回实际值对象的方法之一是使用 get() 方法
+    @Test
+    public void whenCreateOfNullableOptional_thenOk() {
+        String name = "John";
+        Optional<String> opt = Optional.ofNullable(name);
+
+        assertEquals("John", opt.get());
+    }
+
+    // 上面这个方法会在值为 null 的时候抛出异常。要避免异常，你可以选择首先验证是否有值：
+    @Test
+    public void whenCheckIfPresent_thenOk() {
+        User user = new User("john@gmail.com", "1234");
+        Optional<User> opt = Optional.ofNullable(user);
+        assertTrue(opt.isPresent());
+
+        assertEquals(user.getEmail(), opt.get().getEmail());
+    }
+
+
+    //    返回默认值
+    @Test
+    public void whenEmptyValue_thenReturnDefault() {
+        User user = null;
+        User user2 = new User("anna@gmail.com", "1234");
+        User result = Optional.ofNullable(user).orElse(user2);
+
+        assertEquals(user2.getEmail(), result.getEmail());
+    }
+
+    // 如果对象的初始值不是 null，那么默认值会被忽略
+    @Test
+    public void whenValueNotNull_thenIgnoreDefault() {
+        User user = new User("john@gmail.com","1234");
+        User user2 = new User("anna@gmail.com", "1234");
+        User result = Optional.ofNullable(user).orElse(user2);
+
+        assertEquals("john@gmail.com", result.getEmail());
+    }
+
+
+
+
 }
---
+++ src/main/java/com/evan/core/base/optionalDemo/User.java
@@ -0,0 +1,13 @@
+package com.evan.core.base.optionalDemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String email;
+    private String name;
+
+}
