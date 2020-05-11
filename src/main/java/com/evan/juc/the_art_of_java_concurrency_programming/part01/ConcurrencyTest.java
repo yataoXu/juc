89d11d824fa7172de1ed677642a4cfae8be5637b
evan <xuyatao@seassoon.com>
2020-05-11 08:29:40 +0800

    juc

--- src/main/java/com/evan/juc/the_art_of_java_concurrency_programming/part01/ConcurrencyTest.java
+++ src/main/java/com/evan/juc/the_art_of_java_concurrency_programming/part01/ConcurrencyTest.java
@@ -2,32 +2,44 @@ package com.evan.juc.the_art_of_java_concurrency_programming.part01;
 
 public class ConcurrencyTest {
 
-    private static final int count = 100001;
+    private static final int count = 1000000000;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
     }
 
     private static void concurrency() throws InterruptedException {
-
-        long start = System.nanoTime();
-
-        Thread thread = new Thread(() -> {
-
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(() ->
+        {
             int a = 0;
-            for (int i = 0; i < count; i++) {
+            for (long i = 0; i < count; i++) {
                 a += 5;
             }
+            System.out.println(a);
         });
-
         thread.start();
         int b = 0;
-        for (int i = 0; i < count; i++) {
+        for (long i = 0; i < count; i++) {
             b--;
         }
+        long time = System.currentTimeMillis() - start;
         thread.join();
-        long time = System.nanoTime() - start;
-        System.out.println();
+        System.out.println("concurrency :" + time + "ms,b=" + b);
+    }
 
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial:" + time + "ms,b=" + b + ",a=" + a);
     }
 }
